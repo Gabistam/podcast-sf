451b67314ea7ad5d086b0c53ec9e5271793d78d6
gabistam <gabistam@gmail.com>
2022-11-15 03:32:17 +0100

    init BrancheA

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,45 +2,99 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\podcast;
+use App\Entity\Category;
+use App\Entity\Comment;
+use App\Entity\Media;
+use App\Entity\Menu;
+use App\Entity\Option;
+use App\Entity\Page;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+    private $adminUrlGenerator;
+    
+    public function __construct(AdminUrlGenerator $adminUrlGenerator) {
+        $this->adminUrlGenerator = $adminUrlGenerator;
+    }
+
+    /**
+     * @IsGranted("ROLE_AUTHOR")
+     */
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('login');
+        }
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        $controller = $this->isGranted('ROLE_ADMIN') ? MenuCrudController::class : podcastCrudController::class;
 
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
+        $url = $this->adminUrlGenerator
+            ->setController($controller)
+            ->generateUrl();
 
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Podcast Sf');
+            ->setTitle('Pentiminax CMS')
+            ->renderContentMaximized();
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToRoute('Aller sur le site', 'fas fa-undo', 'home');
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+            yield MenuItem::subMenu('Menus', 'fas fa-list')->setSubItems([
+                MenuItem::linkToCrud('Pages', 'fas fa-file', Menu::class),
+                MenuItem::linkToCrud('podcasts', 'fas fa-newspaper', Menu::class),
+                MenuItem::linkToCrud('Liens personnalisés', 'fas fa-link', Menu::class),
+                MenuItem::linkToCrud('Catégories', 'fab fa-delicious', Menu::class),
+            ]);
+        }
+
+        if ($this->isGranted('ROLE_AUTHOR')) {
+            yield MenuItem::subMenu('podcasts', 'fas fa-newspaper')->setSubItems([
+                MenuItem::linkToCrud('Tous les podcasts', 'fas fa-newspaper', podcast::class),
+                MenuItem::linkToCrud('Ajouter', 'fas fa-plus', podcast::class)->setAction(Crud::PAGE_NEW),
+                MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class)
+            ]);
+
+            yield MenuItem::subMenu('Médias', 'fas fa-photo-video')->setSubItems([
+                MenuItem::linkToCrud('Médiathèque', 'fas fa-photo-video', Media::class),
+                MenuItem::linkToCrud('Ajouter', 'fas fa-plus', Media::class)->setAction(Crud::PAGE_NEW),
+            ]);
+        }
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+            yield MenuItem::subMenu('Pages', 'fas fa-file')->setSubItems([
+                MenuItem::linkToCrud('Toutes les pages', 'fas fa-file', Page::class),
+                MenuItem::linkToCrud('Ajouter une page', 'fas fa-plus', Page::class)->setAction(Crud::PAGE_NEW)
+            ]);
+
+            yield MenuItem::linkToCrud('Commentaires', 'fas fa-comment', Comment::class);
+
+            yield MenuItem::subMenu('Comptes', 'fas fa-user')->setSubItems([
+                MenuItem::linkToCrud('Tous les comptes', 'fas fa-user-friends', User::class),
+                MenuItem::linkToCrud('Ajouter', 'fas fa-plus', User::class)->setAction(Crud::PAGE_NEW)
+            ]);
+
+            yield MenuItem::subMenu('Réglages', 'fas fa-cog')->setSubItems([
+                MenuItem::linkToCrud('Général', 'fas fa-cog', Option::class),
+            ]);
+        }
     }
 }
